c3f01a0f1d7fd706e0fba84ef78b3008fc66c8cd
jhonathan <jhonathan.luizferreira@gmail.com>
2018-11-29 00:28:50 -0200

    Toggle led

--- src/app/Components/cards/cards.component.ts
+++ src/app/Components/cards/cards.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ApiService } from 'src/app/Services/api.service';
+import { state } from '@angular/animations';
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
@@ -11,6 +12,7 @@ export class CardsComponent implements OnInit {
   @Input() content: string;
   @Input() suffix = '';
   @Input() type = 0;
+  checked = false;
 
   constructor(
     private apiService: ApiService
